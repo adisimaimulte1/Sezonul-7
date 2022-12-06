3f2002ca558804e0e179a62bbf6ef1ffacd249f7
Clever Core <84577856+clevercore2000@users.noreply.github.com>
2022-12-06 20:48:19 +0200

    AntiG Activ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOp_Test_S7.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOp_Test_S7.java
@@ -15,21 +15,43 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.util.Encoder;
 
 import com.qualcomm.robotcore.hardware.*;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(group = "drive")
 
 public class TeleOp_Test_S7 extends LinearOpMode {
+    double liftSensivity=0.075, angleCot=0.5, movementSensitivity=0.6, grip=0, powerUmar=0, angleUmar, resistanceSensivity=0.04;
     double mls(double x) //modul de liniaritate si sens
     {
-        return x*x*x;
+        return x*x*x*movementSensitivity;
     }
-    double liftSensivity=0.075;
-    double lowerSensitivity=0.05;
+    double liftPower (double x) //ecuatia miscarii ascendente a bratului
+    {
+        x=x/10;
+        if (x<=8) {
+            double y = -0.0027 * x * x * x * x + 0.0422 * x * x * x - 0.1883 * x * x + 10;
+            return y*liftSensivity;
+        }
+        else
+            return 0;
+    }
+    double resistivePower(double x) //ecuatia anti-graviationala
+    {
+        x=x/10;
+        double y=-0.0124*x*x*x*x+0.3152*x*x*x-2.5885*x*x+6.496*x+5;
+        if (x>1)
+            return y*resistanceSensivity;
+        else
+            return 0;
+    }
+    double AngleInMotion (double x) //ecuatia dependentei pozitiei cotului in functie de unghiul umarului
+    {
+        if (x >= 90)
+            return 0.675;
+        else
+            return 0.3;
+    }
     @Override
     public void runOpMode() throws InterruptedException {
-        double sensivity=0.6, position=0.3, movementSensitivity=-0.6, grip=0, liftP=0, lowerP=0;
-        boolean u1=true, u2=true, s1=true, s2=true;
         DcMotor umard = hardwareMap.dcMotor.get("umarDreapta");
         DcMotor umars= hardwareMap.dcMotor.get("umarStanga");
         DcMotor holder = hardwareMap.dcMotor.get("motorCentral");
@@ -48,104 +70,67 @@ public class TeleOp_Test_S7 extends LinearOpMode {
             //movement
             drive.setWeightedDrivePower(
                     new Pose2d(
-                            mls(gamepad1.left_stick_y) * movementSensitivity,
-                            mls(gamepad1.left_stick_x) * movementSensitivity,
-                            mls(gamepad1.right_stick_x) * movementSensitivity
+                            mls(gamepad1.left_stick_y),
+                            mls(gamepad1.left_stick_x),
+                            mls(gamepad2.right_stick_x)
                     )
             );
 
-            //one push
-            //pozitie antebrat
+            angleUmar=(double)holder.getCurrentPosition()*360/8192;
+
             if (gamepad2.right_bumper)
             {
-                lowerSensitivity+=0.005;
+                liftSensivity+=0.005;
                 Thread.sleep(500);
             }
             if (gamepad2.left_bumper)
             {
-                lowerSensitivity-=0.005;
+                liftSensivity-=0.005;
                 Thread.sleep(500);
             }
-            /*if (gamepad2.dpad_up)
+            if (gamepad2.right_trigger>0)
             {
-                position+=0.05;
+                resistanceSensivity+=0.005;
                 Thread.sleep(500);
             }
-            if (gamepad2.dpad_down)
+            if (gamepad2.left_trigger>0)
             {
-                position-=0.05;
+                resistanceSensivity-=0.005;
                 Thread.sleep(500);
-            }*/
-            telemetry.addData("Pozitie servo cot:", position);
-            telemetry.addData("Unghi servo cot:", position*270);
+            }
 
             //control gripper
             if (gamepad2.y) // pick
                 grip=0;
             if (gamepad2.x) // drop
                 grip=0.3;
+
             //lift
             if(gamepad2.left_stick_y<-0.2)
-                liftP = liftPower((double) holder.getCurrentPosition() * 360 / 8192);
+                powerUmar=liftPower(angleUmar);
             else if(gamepad2.left_stick_y>0.2)
-            {
-                liftP=antiG((double)holder.getCurrentPosition()*360/8192);
-            }
+                powerUmar=resistivePower(angleUmar);
             else
-                liftP = 0;
-                umard.setPower(liftP);
-                umars.setPower(liftP);
-                holder.setPower(-liftP);
-            //lower
+                powerUmar=0;
 
-
-
-            position=liftAngle((double)holder.getCurrentPosition()*360/8192);
-
-            telemetry.addData("y ", gamepad2.left_stick_y);
-            telemetry.addData("power ", liftPower((double)holder.getCurrentPosition()*360/8192));
-            telemetry.addData("power2 ", antiG((double)holder.getCurrentPosition()*360/8192));
+            //atribuire pozitie absoluta coate
+            //angleCot=AngleInMotion(angleUmar);
 
             //control si atribuire
-            cotd.setPosition(position);
-            cots.setPosition(position);
+            umard.setPower(powerUmar);
+            umars.setPower(powerUmar);
+            holder.setPower(-powerUmar);
+            cotd.setPosition(angleCot);
+            cots.setPosition(angleCot);
             gripper.setPosition(grip);
 
-
-            //indicatii
-            telemetry.addData("encoder ", (double)holder.getCurrentPosition()*360/8192);
+            telemetry.addData("Cot:", angleCot);
+            telemetry.addData("Umar:", angleUmar);
+            telemetry.addData("liftS", liftSensivity);
+            telemetry.addData("antiG", resistanceSensivity);
+            telemetry.addData("Sensivitate Brat:", liftSensivity);
             telemetry.update();
         }
 
     }
-    double liftPower (double x)
-    {
-        x=x/10;
-        double y=-0.0026*x*x*x*x+0.028*x*x*x-0.039*x*x-0.3753*x+10;
-        if (x<=10.5)
-            return y*liftSensivity;
-        else
-            return 0;
-    }
-    double liftAngle (double x)
-    {
-        if (x<=55){
-            return 0.25;
-        }
-        else if (x>55 && x<110){
-            return (x-55)*0.0073+0.27;
-        }
-        else { return 0.675;}
-
-    }
-    double antiG (double x)
-    {
-        x=x/10;
-        double y=-0.0066*x*x*x*x+0.1992*x*x*x-1.9275*x*x+5.5915*x+5;
-        if (x>=0)
-            return y*lowerSensitivity;
-        else
-            return 0;
-    }
-
 }
